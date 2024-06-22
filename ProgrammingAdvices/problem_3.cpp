1944a6652a1a9bba3549f08b0ed8c48b932a8a62
Abdelrahman74S <bdalrhmnr927@gmail.com>
2024-06-23 01:12:59 +0300

    5

--- ProgrammingAdvices/problem_3.cpp
+++ ProgrammingAdvices/problem_3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
-
+int sum(int a,int b) {
+    return a+b;
+}
 int main(){
-cout<<"abdo, nour";
+    int x =2,y=89;
+    cout<<sum(x,y);
+    cout<<"\n";
+    cout<<"abdo, nour";
 }
